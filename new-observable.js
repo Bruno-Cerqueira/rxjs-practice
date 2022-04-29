ba1fea991ba0b7bba0e420c4e854f57bad414a2d
BrunoMCerqueira <brunocerqueirasd@gmail.com>
2022-04-28 22:55:08 -0300

    feat: Implement multiple functions on pipe params and take function to control subscribe.

--- new-observable.js
+++ new-observable.js
@@ -1,48 +1,78 @@
-let Subject = {
-  _baseFunctions: [],
-  _state: 0,
-  _observers: [],
-  subscribe: function (observer) {
-    this._observers.push(observer);
-  },
-  getState: function () {
-    return this._state;
-  },
-  callSubscribers: function (value) {
-    this._state = value;
-    for (let i = 0; i < this._observers.length; i++) {
-      this._baseFunctions[0](this)
-      this._observers[i].next(this);
+let subject = () => {
+  return { 
+    _baseFunctions: [],
+    _state: 0,
+    _observers: [],
+    _takes: 0,
+    _limit: false,
+    subscribe: function (observer) {
+      this._observers.push(observer);
+    },
+    getState: function () {
+      return this._state;
+    },
+    callSubscribers: function (value) {
+      this._state = value;
+      for (let i = 0; i < this._observers.length; i++) {
+        this.executeBaseFunctions();
+        if(this._takes > 0) this._takes--;
+        this._observers[i].next(this);
+        if(this._takes === 0 && this._limit) this._observers = [];
+      }
+    },
+    pipe: function (...baseFunction) {
+      this._baseFunctions.push(...baseFunction);
+      return this;
+    },
+    executeBaseFunctions: function() {
+      this._baseFunctions.map(baseFunction => {
+        baseFunction(this);
+      })
     }
-  },
-  pipe: function (baseFunction) {
-    this._baseFunctions.push(baseFunction);
-    return this;
-  },
+  }
 };
 
 // BaseFunctions
 const map = (fn) => {
   return function(subject) {
-    /* console.log(value) */
     subject._state = fn(subject.getState())
   }
 }
 
+const take = (number) => {
+  return function(subject) {
+    if(!subject._limit) {
+      subject._takes = number;
+      subject._limit = true;
+    }
+  }
+}
+
 // SpecificFunctions
-//
+
+
+
+
+// Execute code
 let Observer = {
   next: (subject) => {
     let currentValue = subject.getState();
     console.log(currentValue)
   },
 };
+const stream = subject();
 
-Subject.pipe(map((data) => {
-  return data / 100;
-})).subscribe(Observer)
+stream.pipe(
+  map((data) =>  data / 100),
+  map((data) =>  data * 1000),
+  take(2)
+).subscribe(Observer);
 
-Subject.callSubscribers(2);
+stream.callSubscribers(2);
+stream.callSubscribers(2);
+stream.callSubscribers(3);
+
+
 
 /*take = function(fn) {
    return (number) =>{
